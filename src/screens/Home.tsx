ba9b3233640974787bbc67a026457fa15ae52ef0
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-08-12 18:05:49 -0300

    feat: home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,88 @@
+import {
+  Heading,
+  HStack,
+  IconButton,
+  useTheme,
+  VStack,
+  Text,
+  FlatList,
+  Center,
+} from "native-base";
+import { ChatTeardropText, SignOut } from "phosphor-react-native";
+import { useState } from "react";
+
+import Logo from "../assets/logo_secondary.svg";
+import { Filter } from "../components/Filter";
+import { Order, OrderProps } from "../components/Order";
+
+export function Home() {
+  const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
+    "open"
+  );
+  const [orders, setOrders] = useState<OrderProps[]>([]);
+
+  const { colors } = useTheme();
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignContent="center"
+        >
+          <Heading color="gray.100">Solicitações</Heading>
+          <Text color="gray.200">{orders.length}</Text>
+        </HStack>
+
+        <HStack space={3} mb={8}>
+          <Filter
+            type="open"
+            title="Em andamento"
+            onPress={() => setStatusSelected("open")}
+            isActive={statusSelected === "open"}
+          />
+          <Filter
+            type="closed"
+            title="Finalizadas"
+            onPress={() => setStatusSelected("closed")}
+            isActive={statusSelected === "closed"}
+          />
+        </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 50 }}
+          ListEmptyComponent={() => (
+            <Center mt={10}>
+              <ChatTeardropText size={40} color={colors.gray[300]} />
+              <Text color="gray.300" fontSize="lg" mt={5} textAlign="center">
+                Você ainda não possui{"\n"}
+                solicitações{" "}
+                {statusSelected === "open" ? "em andamento" : "finalizadas"}
+              </Text>
+            </Center>
+          )}
+        />
+      </VStack>
+    </VStack>
+  );
+}
